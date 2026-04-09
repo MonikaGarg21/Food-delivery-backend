a9f201983ac047fe49859a24b6e63892b9d9a0a0
Monika Garg <mghighcreations@gmail.com>
2026-04-09 22:32:22 +0530

    food-list adding and removing functionality

---
+++ models/foodModel.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+
+// Schema for food-model:
+const foodSchema = new mongoose.Schema({
+    name: {type:String, required:true},
+    description: {type:String, required:true},
+    price: {type:Number, required:true},
+    image: {type:String, required: true},
+    category: {type:String, required:true}
+})
+
+
+// food-model for project:
+const foodModel = mongoose.models.food || mongoose.model("food", foodSchema)
+
+export default foodModel;
--- server.js
+++ server.js
@@ -1,1 +1,34 @@
-console.log("hello!");
+import express, { response } from "express"
+import cors from "cors"
+import { connectDB } from "./config/db.js"
+import foodRouter from "./routes/foodRoute.js"
+
+
+
+
+
+// app config
+const app = express()
+const port = 4000
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+// db connection
+connectDB();
+
+// api end points
+app.use("/api/food",foodRouter)
+app.use("/images", express.static('uploads'))
+
+app.get("/",(req,res)=>{
+    res.send("API Working")
+})
+
+app.listen(port, ()=>{
+    console.log(`Server Started on http://localhost:${port}`)
+})
+
+
+
